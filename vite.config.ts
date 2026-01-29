0b6688e3af199ec2006c98208949e72c8ef7a922
Rosie <axmedovarobiya16@gmail.com>
2026-01-29 12:08:32 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { componentTagger } from "lovable-tagger";
 import path from "path";
+import { componentTagger } from "lovable-tagger";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    hmr: { overlay: false },
+    hmr: {
+      overlay: false,
+    },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
